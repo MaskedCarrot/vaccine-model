67cca59b7c18595869b7d3fd0b93dbc455652e49
MaskedCarrot <2000apoorv@gmail.com>
2021-04-15 19:28:42 +0530

    update

--- scripts/twitter.py
+++ scripts/twitter.py
@@ -1,8 +1,4 @@
-api_secret_key = 'h8t7UR1kp9gM3mKUjfbKEkrbs404m8E32dZFaFzFT05nikCyBY'
-api_key = 'ML0FGGXhhVEuFJBTmUQUAw3Qu'
-bearer_token = 'AAAAAAAAAAAAAAAAAAAAAA6yLwEAAAAAZLm6jlnzVQhGZ10avjZDKs9uDlg%3DLz8be6Xr4qihErsglUNLArtPt6Vi8Pla8WZMTmZPXoiwB50svZ'
-access_token = '1238526226861449216-xN9w63xo9EokhtV5Hus2YdUbpi7w06'
-access_secret_token = 'GCYbD2ImidmnEUgRLeXugi1DBQM4x0lOfiN61HOa1a6n0'
+
 
 import tweepy
 
